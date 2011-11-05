4b25e62b98b91b8d7d2c091a1fbcc3ba09f09f2a
Dane Elwell <dane.elwell@gmail.com>
2011-11-05 14:35:42 +0000

    Fixes for edge cases. Fails gracefully on 404. Added info to top of file. Pauses half a second between requests to CVE_BASE_URL.

--- rhsa.py
+++ rhsa.py
@@ -1,30 +1,54 @@
+#!/usr/bin/env python -OO
 # CVE -> RHSA Report Generator
+#
+# Requires Beautiful Soup: http://www.crummy.com/software/BeautifulSoup/
+# Currently only tested with Python 2.6, but no reason it shouldn't work
+# with older Python versions (minimum 2.3). Not compatible with Python 3.
+#
+# Use like: ./rhsa.py < cvelist.txt, where cvelist.txt is a whitespace
+# separated list of CVE numbers in the format CVE-YYYY-XXXX.
+#
+# This will find the CVE on the CVE_BASE_URL site and scrape for the
+# related RHSA. If it can't find the CVE, chances are it doesn't affect
+# Red Hat or Linux. If it can't find an RHSA, then it'll be something
+# they don't intend to fix, so output the statement from Red Hat. 
+# Otherwise, consider resolved and output the link to the RHSA.
+# This of course assumes you ARE running the latest CentOS/RHEL release 
+# versions of the software you're checking the CVEs for.
+#
+# No guarantees anything this outputs is correct or proper.
 
 import sys
 import re
 import urllib2
+from time import sleep
 from BeautifulSoup import BeautifulSoup
 
 CVE_BASE_URL = "https://www.redhat.com/security/data/cve/"
+RHEL_VERSION = "5"
 
 def get_cve_info(cve):
         cveurl = CVE_BASE_URL + cve + ".html"
         try:
             html = urllib2.urlopen(cveurl).read()
-        except HTTPError:
+        except urllib2.HTTPError:
             # 404 or general screwup
-            return cve + " -- Not found on Red Hat's website. Google it, might be Windows only."
+            return cve + " -- !!FIX!! Not found on Red Hat's website. Google it, might be Windows only."
 
         soup = BeautifulSoup(html)
 
-        if soup.find(text=re.compile(".*Red Hat Enterprise Linux version 5.*")) is not None:
+        r = re.compile(".*Red Hat Enterprise Linux version "+RHEL_VERSION+".*")
+
+        if soup.find(text=r) is not None:
             # If we've found the above, we have an RHSA (in theory!)
-            rhsa = soup.find(text=re.compile(".*Red Hat Enterprise Linux version 5.*")).findNext('a')['href']
+            rhsa = soup.find(text=r).findNext('a')['href']
             return cve + " -- Resolved: " + rhsa
 
-        else:
+        elif soup.find(text="Statement"):
             statement = ' '.join([text for text in soup.find(text="Statement").findNext('p').findAll(text=True)])
             return cve + " -- Red Hat Statement: "+ cveurl +": \""+ statement + "\""
+        else:
+            return cve + " -- !!FIX!! No RHSA for version "+RHEL_VERSION+", no statement either."
 
 
 if __name__ == '__main__':
@@ -33,6 +57,7 @@ if __name__ == '__main__':
 
     if sys.stdin.isatty():
         print "No input detected. You need to pipe a whitespace separated list of CVEs in!"
+        print "e.g. `./rhsa.py < cvelist.txt` or your preferred method."
         sys.exit(1)
     else:
         rawdata = sys.stdin.read()
@@ -41,3 +66,4 @@ if __name__ == '__main__':
 
     for cve in cves:
         print get_cve_info(cve)
+        sleep(0.5) # to be nice!
