bf54651304705d88ad93bccf5bef7dd995fb2544
Dane Elwell <dane.elwell@gmail.com>
2011-11-05 03:55:48 +0000

    Doesn't use a database anymore, just scrapes everything from the RedHat website!

--- rhsa.py
+++ rhsa.py
@@ -1,25 +1,11 @@
 # CVE -> RHSA Report Generator
 
-import MySQLdb
 import sys
+import re
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-
 CVE_BASE_URL = "https://www.redhat.com/security/data/cve/"
-
-rawdata = ""
-
-if sys.stdin.isatty():
-    print "No input detected. You need to pipe a whitespace separated list of CVEs in!"
-    sys.exit(1)
-else:
-    rawdata = sys.stdin.read()
-
-cves = rawdata.split()
-
-conn = MySQLdb.connect(host="localhost",user="cve",passwd="bluem00n",db="cve_rhsa")
-c = conn.cursor()
 
 def get_cve_info(cve):
         cveurl = CVE_BASE_URL + cve + ".html"
@@ -28,15 +14,30 @@ def get_cve_info(cve):
         except HTTPError:
             # 404 or general screwup
             return cve + " -- Not found on Red Hat's website. Google it, might be Windows only."
-        
-        soup = BeautifulSoup(html)
-        statement = ' '.join([text for text in soup.find(text="Statement").findNext('p').findAll(text=True)])
-        return cve + " -- Red Hat Statement: "+ cveurl +": \""+ statement + "\""
 
-for cve in cves:
-    c.execute("""SELECT rhsa FROM cverhsa WHERE cve=%s""", (cve,))
-    rhsalink = c.fetchone()
-    if not rhsalink == None:
-        print cve + " -- Resolved: " + rhsalink[0]
+        soup = BeautifulSoup(html)
+
+        if soup.find(text=re.compile(".*Red Hat Enterprise Linux version 5.*")) is not None:
+            # If we've found the above, we have an RHSA (in theory!)
+            rhsa = soup.find(text=re.compile(".*Red Hat Enterprise Linux version 5.*")).findNext('a')['href']
+            return cve + " -- Resolved: " + rhsa
+
+        else:
+            statement = ' '.join([text for text in soup.find(text="Statement").findNext('p').findAll(text=True)])
+            return cve + " -- Red Hat Statement: "+ cveurl +": \""+ statement + "\""
+
+
+if __name__ == '__main__':
+
+    rawdata = ""
+
+    if sys.stdin.isatty():
+        print "No input detected. You need to pipe a whitespace separated list of CVEs in!"
+        sys.exit(1)
     else:
+        rawdata = sys.stdin.read()
+
+    cves = rawdata.split()
+
+    for cve in cves:
         print get_cve_info(cve)
