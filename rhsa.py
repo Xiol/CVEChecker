946077b9907d493ba667ea0020930488295ea921
Dane Elwell <dane.elwell@gmail.com>
2011-11-05 02:39:18 +0000

    Now retrieves data for CVEs that don't have associated RHSA data from the RH CVE website and displays the statement from Red Hat regarding why the issue hasn't been fixed!

--- rhsa.py
+++ rhsa.py
@@ -2,6 +2,11 @@
 
 import MySQLdb
 import sys
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+
+CVE_BASE_URL = "https://www.redhat.com/security/data/cve/"
 
 rawdata = ""
 
@@ -16,10 +21,22 @@ cves = rawdata.split()
 conn = MySQLdb.connect(host="localhost",user="cve",passwd="bluem00n",db="cve_rhsa")
 c = conn.cursor()
 
+def get_cve_info(cve):
+        cveurl = CVE_BASE_URL + cve + ".html"
+        try:
+            html = urllib2.urlopen(cveurl).read()
+        except HTTPError:
+            # 404 or general screwup
+            return cve + " -- Not found on Red Hat's website. Google it, might be Windows only."
+        
+        soup = BeautifulSoup(html)
+        statement = ' '.join([text for text in soup.find(text="Statement").findNext('p').findAll(text=True)])
+        return cve + " -- Red Hat Statement: "+ cveurl +": \""+ statement + "\""
+
 for cve in cves:
     c.execute("""SELECT rhsa FROM cverhsa WHERE cve=%s""", (cve,))
     rhsalink = c.fetchone()
     if not rhsalink == None:
         print cve + " -- Resolved: " + rhsalink[0]
     else:
-        print cve + " -- Not found. Please confirm with Google!"
+        print get_cve_info(cve)
