471d4eaa57bf306e3f61b4967e41a786503b6986
Dane Elwell <dane.elwell@gmail.com>
2011-11-12 02:44:20 +0000

    Results are now cached in a sqlite3 database for speedier repeat lookups

--- rhsa.py
+++ rhsa.py
@@ -21,6 +21,8 @@
 import sys
 import re
 import urllib2
+import sqlite3
+import os
 from time import sleep
 from BeautifulSoup import BeautifulSoup
 
@@ -29,17 +31,34 @@ RHEL_VERSION = "5"
 
 rhsa_r = re.compile(".*Red Hat Enterprise Linux version "+RHEL_VERSION+".*")
 
+curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
+
+conn = sqlite3.connect(os.path.join(curdir, 'cache.db'))
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS cache (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, cve TEXT NOT NULL, result TEXT NOT NULL)")
+cur.execute("CREATE INDEX IF NOT EXISTS cve_idx ON cache (cve)")
+conn.commit()
+cur.close()
+
 def get_cve_info(cve, platform='x86_64'):
     if platform not in ['x86_64','i386']:
         return "Platform must be 'x86_64' or 'i386'."
 
     cve = cve.strip()
+
+    cachechk = _retr_cve(cve)
+    if cachechk is not None:
+        return cachechk
+
     cveurl = CVE_BASE_URL + cve + ".html"
     try:
         html = urllib2.urlopen(cveurl).read()
     except urllib2.HTTPError:
         # 404 or general screwup
-        return cve + " -- !!FIX!! Not found on Red Hat's website. Google it, might be Windows only or bad CVE reference."
+        result = "!!FIX!! Not found on Red Hat's website. Google it, might be Windows only or bad CVE reference."
+        _add_cve(cve, result)
+        return cve + " -- " + result
     except urllib2.URLError:
         return
 
@@ -51,13 +70,33 @@ def get_cve_info(cve, platform='x86_64'):
         rhsa_soup = BeautifulSoup(urllib2.urlopen(rhsa).read())
         ver = rhsa_soup.find('a',attrs={"name": "Red Hat Enterprise Linux (v. "+RHEL_VERSION+" server)"}).findNext(text="SRPMS:").findNext('td').contents[0]
         ver = ver.replace(".src.", '.'+platform+'.')
-        return cve + " -- Resolved in version "+ver+": " + rhsa
+        result = "Resolved in version "+ver+": " + rhsa
+        _add_cve(cve, result)
+        return cve + " -- " + result
     elif soup.find(text="Statement"):
         statement = ' '.join([text for text in soup.find(text="Statement").findNext('p').findAll(text=True)])
-        return cve + " -- Red Hat Statement: \""+ statement + "\" - " + cveurl
+        result = "Red Hat Statement: \""+ statement + "\" - " + cveurl
+        _add_cve(cve, result)
+        return cve + " -- " + result
     else:
-        return cve + " -- !!FIX!! No RHSA for version "+RHEL_VERSION+", no statement either. See: " + cveurl
+        result = "!!FIX!! No RHSA for version "+RHEL_VERSION+", no statement either. See: " + cveurl
+        _add_cve(cve, result)
+        return cve + " -- " + result
 
+def _add_cve(cve, result):
+    cur = conn.cursor()
+    cur.execute("""INSERT INTO cache(cve, result) VALUES (?, ?)""", (cve, result,))
+    conn.commit()
+    cur.close()
+
+def _retr_cve(cve):
+    cur = conn.cursor()
+    cur.execute("""SELECT cve,result FROM cache WHERE cve=? LIMIT 1""", (cve,))
+    result = cur.fetchone()
+    cur.close()
+    if result is not None:
+        result =  ' -- '.join([t for t in result if t is not None])
+    return result
 
 if __name__ == '__main__':
     rawdata = ""
@@ -73,4 +112,3 @@ if __name__ == '__main__':
 
     for cve in cves:
         print get_cve_info(cve)
-        sleep(0.3) # to be nice!
